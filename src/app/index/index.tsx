d03f68ac388551a32f228cf09d6df6d9a2586bbb
waltbcc <waltbcc@gmail.com>
2026-07-22 20:27:20 -0300

    View

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -2,8 +2,9 @@ import { View, Text } from 'react-native'
 
 export default function Home() {
   return (
-    <View>
-      <Text>Ola</Text>
+    <View style={{flex: 1, backgroundColor: 'plum'}}>
+      <View style={{width: 200, height: 200, backgroundColor: 'lightgreen'}}></View>
+      <View style={{width: 200, height: 200, backgroundColor: 'lightblue'}}></View>
     </View>
   )
 }
