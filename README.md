7afdf3d8c682a08e36f67a4912af80881b41f245
waltbcc <waltbcc@gmail.com>
2026-07-22 20:36:27 -0300

    Text

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 ### Componentes básicos
 
 - View
+- Text
--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -2,9 +2,8 @@ import { View, Text } from 'react-native'
 
 export default function Home() {
   return (
-    <View style={{flex: 1, backgroundColor: 'plum'}}>
-      <View style={{width: 200, height: 200, backgroundColor: 'lightgreen'}}></View>
-      <View style={{width: 200, height: 200, backgroundColor: 'lightblue'}}></View>
+    <View style={{flex: 1, backgroundColor: 'plum', padding: 60}}>
+      <Text style={{fontSize: 20, fontWeight: '600'}}><Text style={{color: 'white'}}>Hello</Text> World</Text>
     </View>
   )
 }
