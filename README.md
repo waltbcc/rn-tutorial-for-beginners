532d9c47a52f1c64a5e13bd0f618c2c2affe5136
waltbcc <waltbcc@gmail.com>
2026-07-22 20:55:00 -0300

    ImageBackground

--- README.md
+++ README.md
@@ -11,3 +11,4 @@
 - View
 - Text
 - Image
+- ImageBackground
--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,12 +1,13 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, ImageBackground } from 'react-native'
 
 const logoImag = require('../../../assets/img.jpg');
 
 export default function Home() {
   return (
     <View style={{flex: 1, backgroundColor: 'plum', padding: 60}}>
-      <Image source={logoImag} style={{width: 300, height: 300}} />
-      <Image source={{uri: 'https://picsum.photos/300'}} style={{width: 300, height: 300}} />
+      <ImageBackground source={logoImag} style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Texto Imagem</Text>
+      </ImageBackground>
     </View>
   )
 }
