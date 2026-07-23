146982eb38506516349044403bee3c3556d3c8d3
waltbcc <waltbcc@gmail.com>
2026-07-23 14:38:01 -0300

    Status Bar

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, ImageBackground, ScrollView, Pressable, Button, Modal } from 'react-native'
+import { View, Text, Image, ImageBackground, ScrollView, Pressable, Button, Modal, StatusBar } from 'react-native'
 import { useState } from 'react';
 
 const logoImage = require('../../../assets/img.jpg');
@@ -7,22 +7,7 @@ export default function Home() {
   const [isModalVisible, setModalVisible]=useState(false);
   return (
     <View  style={{flex: 1, backgroundColor: 'plum', padding: 60}}>
-      <Button 
-      title="Avançar"
-      color="red"
-      onPress={() => (setModalVisible(true))}
-      />
-      <Modal 
-        visible={isModalVisible}
-        animationType='slide'
-        onRequestClose={()=>(setModalVisible(false))}
-        presentationStyle='overFullScreen'
-        style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)'}}>
-          <View style={{backgroundColor: 'white', padding: 20, borderRadius: 8, width: 300, height: 300}}>
-            <Text>Modal aberto</Text>
-            <Button title='fechar modal' onPress={()=> setModalVisible(false)} />
-          </View>
-      </Modal>
+      <StatusBar backgroundColor="red" barStyle="light-content"  />
     </View>
   )
 }
